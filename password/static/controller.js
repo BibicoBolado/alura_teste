b0196e3e097c3b9300851dbb8cff41be5eae6983
bibicobolado <rodrigosiqueira@id.uff.br>
2018-12-28 10:13:40 -0200

    refatoramento js

--- password/static/controller.js
+++ password/static/controller.js
@@ -12,82 +12,128 @@ class RegisterController{
 		this.addEvent();
 	}
 
-	addEvent(){
+	addEvent(){ //Método que adiciona o evento input ao campo de senha e chama a função transforma_vetor
 		this.senha_um.addEventListener('input',()=>{
 			this.transforma_vetor(this.senha_um.value);
 		})
 	}
 
-	transforma_vetor(x){
-		var controle_sim = 0;
-		var controle_let	= 0;
-		var controle_num = 0;
-		var soma	= 0;
-		var tamanho = 0;
-		var vetor = x.split("");
-		var i;
-		for (i = 0; i<vetor.length;i++){
-			var a = this.verifica_senha(vetor[i]);
-			if(a==1){
-				controle_sim 	= 1;
+	transforma_vetor(senha_digitada){
+		var existe_simbolo = 0; //Variável que indica a existência de um símbulo dentro da senha digitada. (!,@,#,$,%,&,*,(,),?,+,-)
+		var existe_letra   = 0; //Variável que indica a existência de uma letra dentro da senha digitada .
+		var existe_numero  = 0; //Variável que indica a existência de um número dentro da senha digitada .
+		var nivel_senha	   = 0; //Variável que   verifica  quantos requisitos  da  senha foram atendidos .
+		var senha_vetor    = senha_digitada.split("");//Transforma a senha digitada em um array
+
+		for (var i = 0; i<senha_vetor.length;i++){
+			var item_senha = this.verifica_senha(senha_vetor[i]); //Verifica se cada Item da Seha é Número, Símbolo ou Variável .
+			if(item_senha==1){
+				existe_simbolo 	= 1; //Existe um número na  senha
 			}
-			if(a==2){
-				controle_let	= 1;
+			else if(item_senha==2){
+				existe_letra  	= 1; //Existe uma letra na  senha
 			}
-			if(a==3){
-				controle_num 	= 1;
+			else if(item_senha==3){
+				existe_numero  	= 1; //Existe um símbolo na senha
 			}
 		}
-		soma = controle_num+controle_let+controle_sim;
-		if(vetor.length>=4 && vetor.length<=7){
-			tamanho 			= 1;
-		}
-		if(vetor.length>=8){
-			tamanho 			= 2;
-		}
-		console.log(vetor)
-		console.log(soma,tamanho);
-		this.altera_status(soma,tamanho);
+		nivel_senha = existe_numero + existe_letra  + existe_simbolo; // Verifica o nível da senha
+		console.log(senha_vetor)
+		console.log(nivel_senha,senha_vetor.length );
+		this.altera_status(nivel_senha,senha_vetor.length ); 
+		//Modifica o DOM, mostrando status da senha ao usuário de acordo com o nivel da senha e o tamanho.
 
 	}
 
-	verifica_senha(x){
+	verifica_senha(item_senha){
 
+		//Lista de símbolos aceitos para tornar a senha mais forte
 		var simbolos	= 	["!","@","#","$","%","&","*","(",")","?","+","-"];
-		if(simbolos.indexOf(x)!=-1){
+
+		//Verifica se o item analisado é algum dos símbolos
+		if(simbolos.indexOf(item_senha)!=-1){
 			return (1);
 		}
 
-		else if(isNaN(x)){
+		//Verifica se o item analisado é alguma letra
+		else if(isNaN(item_senha)){
 			return (2);
-			
+
+		//Verifica se o item analisado é  um   número
 		}else{
 			return (3);
 		}
 	}
 
-	altera_status(x,y){
-		if(y==0){
-			this.nivel_dois.style.display = "none";
-			this.botao.disabled = true;
+	altera_status(nivel_senha,tamanho_senha){
+		if(tamanho_senha<=4){
+			this.trava_tudo();
 		}
-		if (y==1){
-			this.nivel_tres.style.display = "none";
-			this.botao.disabled = true;
-			if(x==1){
-				this.nivel_dois.style.display = "block";	
+		else if (tamanho_senha>=4 && tamanho_senha<=7){
+			if(nivel_senha==1){
+				this.trava_botao();
+				this.senha_fraca();
 			}
-		}else if(y==2){
-			if(x==2){
-				this.botao.disabled = false;
-				this.nivel_quatro.style.display = "none";
-				this.nivel_tres.style.display = "block";
+			else if(nivel_senha==2){
+				this.trava_botao();
+				this.senha_mediana();
 			}
 
-			if(x==3){
-				this.nivel_quatro.style.display = "block";
+			else{
+				this.trava_botao();
+				this.senha_forte();
 			}
 		}
+		else{
+			if(nivel_senha==1){
+				this.trava_botao();
+				this.senha_fraca();
+
+			}
+			else if(nivel_senha==2){
+				this.trava_botao();
+				this.senha_mediana();
+				
+			}
+
+			else{
+				this.libera_botao();
+				this.senha_forte();
+			}
+		}
+	}
+
+	libera_botao(){
+		this.botao.disabled = false;
+	}
+
+	trava_botao(){
+		this.botao.disabled = true;
+	}
+
+	senha_forte(){
+		this.nivel_dois.style.display = "block";
+		this.nivel_tres.style.display = "block";
+		this.nivel_quatro.style.display = "block";		
+	}
+
+	senha_mediana(){
+		this.nivel_dois.style.display = "block";
+		this.nivel_tres.style.display = "block";
+		this.nivel_quatro.style.display = "none";
+	}
+
+	senha_fraca(){
+		this.nivel_dois.style.display = "block";
+		this.nivel_tres.style.display = "none";	
+		this.nivel_quatro.style.display = "none";
+	}
+
+	trava_tudo(){
+		this.botao.disabled = true;
+		this.nivel_dois.style.display = "none";
+		this.nivel_tres.style.display = "none";	
+		this.nivel_quatro.style.display = "none";
 	}
 
 }
